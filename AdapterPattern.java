2b2690b4f1c75f8d32bb4316db0c73ad3178b1b5
M.Mert Usta <jaquinjj@users.noreply.github.com>
2022-06-09 13:50:15 +0300

    Create AdapterPattern.java
    AdapterPattern

---
+++ AdapterPattern.java
@@ -0,0 +1,66 @@
+/* Online Java Compiler and Editor */
+public class HelloWorld{
+    
+    public interface Car{
+        double getWeight();
+    }
+    
+    public static class Mercedes implements Car{
+        @Override
+        public double getWeight(){
+            return 30000;
+        }
+    }
+    
+     public static class BMW implements Car{
+        @Override
+        public double getWeight(){
+            return 40000;
+        }
+    }
+    
+    
+    public interface CarAdapter
+    {
+        double getWeight();
+    }
+    
+    public static class CarAdapterImpl implements CarAdapter
+    {
+        Car car;
+        public CarAdapterImpl(Car car)
+        {
+            this.car = car;
+        }
+        
+        @Override
+        public double getWeight(){
+            return gramsToKilograms(car.getWeight());
+        }
+        
+        
+        public double gramsToKilograms(double weight)
+        {
+            return weight/1000;
+        }
+        
+        
+    }
+    
+    
+    
+    
+     public static void main(String []args){
+         Car mercedes = new Mercedes();
+         Car bmw = new BMW();
+         
+         CarAdapter mercedesCarAdapter = new CarAdapterImpl(mercedes);
+         CarAdapter bmwCarAdapter = new CarAdapterImpl(bmw);
+         
+         
+         
+        System.out.println(mercedesCarAdapter.getWeight());
+        System.out.println(bmwCarAdapter.getWeight());
+
+     }
+}
